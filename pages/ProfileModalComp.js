ac0abe46ddface7c908b8b6c9d0299d3f4315f3e
subham <subham672singh@gmail.com>
2023-10-24 10:39:15 +0530

    pwa support

--- pages/ProfileModalComp.js
+++ pages/ProfileModalComp.js
@@ -1,6 +1,7 @@
 import { Fragment, useRef, useState } from "react";
 import { Dialog, Transition } from "@headlessui/react";
 import { ExclamationTriangleIcon } from "@heroicons/react/24/outline";
+import ProfileForm from "../components/ProfileForm";
 
 export default function ProfileModelComp() {
   const [open, setOpen] = useState(true);
@@ -41,13 +42,13 @@ export default function ProfileModelComp() {
               <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
                 <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
                   <div className="sm:flex sm:items-start">
-                    <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
+                    {/* <div className="mx-auto flex h-12 w-12 flex-shrink-0 items-center justify-center rounded-full bg-red-100 sm:mx-0 sm:h-10 sm:w-10">
                       <ExclamationTriangleIcon
                         className="h-6 w-6 text-red-600"
                         aria-hidden="true"
                       />
-                    </div>
-                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                    </div> */}
+                    {/* <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
                       <Dialog.Title
                         as="h3"
                         className="text-base font-semibold leading-6 text-gray-900"
@@ -61,7 +62,8 @@ export default function ProfileModelComp() {
                           cannot be undone.
                         </p>
                       </div>
-                    </div>
+                    </div> */}
+                    <ProfileForm />
                   </div>
                 </div>
                 <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
