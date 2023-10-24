4137de81406be2a9064d4ddb6b91f05fdd886c82
subham <subham672singh@gmail.com>
2023-10-24 09:55:25 +0530

    updated routes

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,14 @@
 import Head from "next/head";
 import Image from "next/image";
 import styles from "../styles/Home.module.css";
-import Login from "../components/login";
-import ListingComp from "../components/ListingComp";
-import ProfileModelComp from "../components/ProfileModalComp";
+import Login from "./Login";
+import ListingComp from "./ListingComp";
+import ProfileModelComp from "./ProfileModalComp";
 
 export default function Home() {
   return (
     <>
       <Login />;
-      <ListingComp />
-      <ProfileModelComp />
     </>
   );
 }
